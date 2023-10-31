095301f55f7fcb156719c1e4e49c5e1efb44e220
Blade Johnson <davbjohn@ut.utm.edu>
2023-10-30 20:39:46 -0500

    models, movement, camera

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ let renderer;
 
 let clock;
 
-var player;
+var playerMesh;
 var projectileMesh;
 let projectileMeshes = [];
 
 var delay = 0;
 
-let mixers = [];
+var bossMesh;
 
 let keyboard = {};
 
@@ -22,15 +22,41 @@ let keyboard = {};
 //////////////////////////////////////////////////////////
 async function spawnPlayer(){
     const loader = new GLTFLoader();
-    loader.load('models/DroneAttack.glb', function(gltf){
-        player = gltf.scene;
-        scene.add(player);
-    })
+    const player = await loader.loadAsync('models/player.glb');
+    playerMesh = player.scene.children[0];
+    playerMesh.rotateY(45 * Math.PI/2);
+    scene.add(playerMesh);
 }
 //////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////
-// Gets key info
+// Spawns bosses
+//////////////////////////////////////////////////////////
+async function spawnBoss1(){
+    const loader = new GLTFLoader();
+    const boss = await loader.loadAsync('models/moon.glb');
+    bossMesh = boss.scene.children[0];
+    bossMesh.position.y = -15;
+    bossMesh.position.z = -80;
+    bossMesh.rotateY(-89.9);
+    bossMesh.scale.set(35,35,35);
+    scene.add(bossMesh);
+}
+
+async function spawnBoss2(){
+    const loader = new GLTFLoader();
+    const boss = await loader.loadAsync('models/moth.glb');
+    bossMesh = boss.scene.children[0];
+    bossMesh.position.y = 0;
+    bossMesh.position.z = -100;
+    //bossMesh.rotateY(149.9);
+    bossMesh.scale.set(7,7,7);
+    scene.add(bossMesh);
+}
+//////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////
+// Gets keyboard info
 //////////////////////////////////////////////////////////
 function addKeysListener(){
     window.addEventListener('keydown', function(e){
@@ -46,11 +72,35 @@ function addKeysListener(){
 // Player movement
 //////////////////////////////////////////////////////////
 function movePlayer(){
-    if(keyboard[65] && player.position.x > -43){
-        player.position.x -= 0.60;
+    if(keyboard[65] && playerMesh.position.x > -23){
+        playerMesh.position.x -= 0.60;
     }
-    if(keyboard[68] && player.position.x < 43){
-        player.position.x += 0.60;
+    if(keyboard[68] && playerMesh.position.x < 23){
+        playerMesh.position.x += 0.60;
+    }
+    if(keyboard[87] && playerMesh.position.z > -23){
+        playerMesh.position.z -= 0.60;
+    }
+    if(keyboard[83] && playerMesh.position.z < 0){
+        playerMesh.position.z += 0.60;
+    }
+}
+//////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////
+// Camera movement
+//////////////////////////////////////////////////////////
+function moveCamera(){
+    if(keyboard[38]){
+        camera.position.z = -20;
+        camera.position.y = 50;
+        camera.rotation.x = -Math.PI / 2;
+    }
+    if(keyboard[40]){
+        camera.position.z = 30;
+    camera.position.y = 20;
+    camera.position.x = 0;
+    camera.rotation.x = -0.3;
     }
 }
 //////////////////////////////////////////////////////////
@@ -61,18 +111,49 @@ function movePlayer(){
 // Projectile spawning
 //////////////////////////////////////////////////////////
 async function spawnProjectile(){
-    const gltfLoader = new GLTFLoader();
-	const projectileGLTF = await gltfLoader.loadAsync( 'models/Projectile.glb' );
-	projectileMesh = projectileGLTF.scene;
-	projectileMesh.scale.set(1, 1, 1);
+    var rng = Math.round(Math.random() * 12);
+    switch(rng){
+        case 1:
+        case 2:
+        case 3:
+            const gltfLoader1 = new GLTFLoader();
+	        const projectileGLTF1 = await gltfLoader1.loadAsync( 'models/PlayerProj/proj1.glb' );
+	        projectileMesh = projectileGLTF1.scene;
+	        projectileMesh.scale.set(1, 1, 1);
+            break;
+        case 4:
+        case 5:
+        case 6:
+            const gltfLoader2 = new GLTFLoader();
+	        const projectileGLTF2 = await gltfLoader2.loadAsync( 'models/PlayerProj/proj2.glb' );
+	        projectileMesh = projectileGLTF2.scene;
+	        projectileMesh.scale.set(1, 1, 1);
+            break;
+        case 7:
+        case 8:
+        case 9:
+            const gltfLoader3 = new GLTFLoader();
+	        const projectileGLTF3 = await gltfLoader3.loadAsync( 'models/PlayerProj/proj3.glb' );
+	        projectileMesh = projectileGLTF3.scene;
+	        projectileMesh.scale.set(1, 1, 1);
+            break;
+        case 10:
+        case 11:
+        case 12:
+            const gltfLoader4 = new GLTFLoader();
+	        const projectileGLTF4 = await gltfLoader4.loadAsync( 'models/PlayerProj/proj4.glb' );
+	        projectileMesh = projectileGLTF4.scene;
+	        projectileMesh.scale.set(1, 1, 1);
+            break;
+    }
 }
 //////////////////////////////////////////////////////////
 // Projectile movement
 //////////////////////////////////////////////////////////
 function updateProjectiles(){
 	projectileMeshes.forEach((projectile, index) => {
-		projectile.position.z += 0.5;
-		if(projectile.position.z > 40){
+		projectile.position.z -= 0.5;
+		if(projectile.position.z > 23){
 			scene.remove(projectile);
 			projectileMeshes.splice(index, 1);
 		  }
@@ -82,11 +163,12 @@ function updateProjectiles(){
 // Shooting
 //////////////////////////////////////////////////////////
 function shoot(){
+    spawnProjectile();
     if (keyboard[32] && delay == 0) {
         let projectileMeshClone = projectileMesh.clone();
-        projectileMeshClone.position.x = player.position.x;
-        projectileMeshClone.position.y = player.position.y;
-        projectileMeshClone.position.z = player.position.z;
+        projectileMeshClone.position.x = playerMesh.position.x;
+        projectileMeshClone.position.y = playerMesh.position.y;
+        projectileMeshClone.position.z = playerMesh.position.z - 3;
         scene.add(projectileMeshClone);
         projectileMeshes.push(projectileMeshClone);
         delay = 15;
@@ -107,15 +189,13 @@ function updateDelay(){
 //////////////////////////////////////////////////////////
 function animate() {
     movePlayer();
+    moveCamera();
     shoot();
 	updateProjectiles();
     updateDelay();
 
     renderer.render(scene, camera)
     requestAnimationFrame(animate)
-
-    const dt = clock.getDelta();
-	for ( const mixer of mixers ) mixer.update( dt );
 }
 //////////////////////////////////////////////////////////
 
@@ -130,32 +210,34 @@ async function init() {
 
     //Renderer setup
     renderer = new THREE.WebGLRenderer({antialias: true});
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth , window.innerHeight);
+
+
+    document.getElementById("main").appendChild( renderer.domElement );
 
     //Light setup
-    const hlight = new THREE.AmbientLight(0x404040, 100)
+    const hlight = new THREE.HemisphereLight(0xffffff, 0x000000, 10);
+    hlight.position.set(0, 5, 0);
     scene.add(hlight)
 
     const light = new THREE.PointLight(0xffffff, 1000)
     light.position.set(2.5, 7.5, 15)
     scene.add(light)
 
-    //Camera stuff, not final, will be changed when camera moves to keys/mouse
+    //Starting camera
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-    camera.position.y = -50;
-    camera.lookAt( scene.position ); // the origin
-    if (camera.position.z < 0) {
-        camera.rotation.z = 0;
-    }
+    camera.position.z = 30;
+    camera.position.y = 20;
+    camera.position.x = 0;
+    camera.rotation.x = -0.3;
 
     //Funny game clock
     clock = new THREE.Clock();
   
     spawnPlayer();
-    spawnProjectile();
-  
+    spawnBoss2();
   
     addKeysListener();
     animate();
